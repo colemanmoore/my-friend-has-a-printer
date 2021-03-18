2e82a36eeec6693ee1ae7d82b894985085b60944
Coleman Moore <cmoore.jennikayne@gmail.com>
2021-03-18 09:22:33 -0700

    file being uploaded but not transferred yet

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var socket = require('./socketio-connector');
 var router = express.Router();
+var formidable = require('formidable');
+var FileReader = require('filereader');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -22,15 +23,60 @@ router.get('/', (req, res, next) => {
   res.render('index', {});
 });
 
-router.get('/print', (req, res) => {
-  socket.io.emit('my-friend-request-print', req.body);
-  console.log('requested print job', req.body);
-  // res.redirect('/print', {});
+router.post('/print', (req, res, next) => {
+
+  var form = formidable();
+
+  form.parse(req, function(err, fields, files) {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    if (fields.passcode !== process.env.HONEYJAR) {
+      res.render('confirmation', {
+        error: true,
+        message: 'Sorry - ask your friend for the passcode again'
+      });
+      return;
+    }
+    console.log(files)
+
+    var file = files.file_to_print;
+    if (!file) {
+      res.render('confirmation', {
+        error: true,
+        message: 'Sorry, we didn\'t receive your file'
+      });
+      return;
+    }
+
+    console.log((Object.getOwnPropertyNames(file).filter(function (p) {
+      return typeof file[p] === 'function';
+    })));
+
+    otherMethod(file)
+
+    // socket.io.emit('my-friend-request-print', {});
+    res.render('confirmation', { message: 'Your file is being printed!' });
+  });
 });
 
-// router.get('/print', (req, res) => {
-//   res.render('success', {})
-// })
+function otherMethod(file) {
+  var fileReader = new FileReader();
+  var slice = file.slice(0, 100000);
+
+  fileReader.readAsArrayBuffer(slice);
+  fileReader.onload = (evt) => {
+    var arrayBuffer = fileReader.result;
+    socket.emit('upload-file-slice', {
+      name: file.name,
+      type: file.type,
+      size: file.size,
+      data: arrayBuffer
+    });
+  }
+}
 
 app.use(router);
 
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,24 @@
-document.getElementById('myFile').addEventListener('change', function(e) {
+document.getElementById('file-to-print').addEventListener('change', function(e) {
     var filename = e.target.files[0].name;
     var feedbackElem = document.getElementById('file-feedback');
     feedbackElem.appendChild(document.createTextNode(filename));
 });
 
+document.getElementById('passcode').addEventListener('blur', validateInputs);
+
+document.getElementById('file-to-print').addEventListener('blur', validateInputs);
+
+function validateInputs() {
+    var file = document.getElementById('file-to-print');
+    var passcode = document.getElementById('passcode');
+
+    if (
+        file && file.files && file.files[0] &&
+        passcode.value
+    ) {
+        document.getElementById('send-to-printer-button').removeAttribute('disabled');
+
+    } else {
+        document.getElementById('send-to-printer-button').setAttribute('disabled', 'true');
+    }
+}
